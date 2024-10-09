3755fc25bb284c7846ee137ff60b609cdf52a6a1
Artemij Shepelev <ffelian@gmail.com>
2024-10-09 23:22:18 +0300

    changed dummy installer behavior

--- installer/dummy.go
+++ installer/dummy.go
@@ -3,8 +3,8 @@ package installer
 // Dummy is a stub
 type Dummy struct{}
 
-// IsInstalled always return false
-func (d Dummy) IsInstalled() bool { return false }
+// IsInstalled always return true
+func (d Dummy) IsInstalled() bool { return true }
 
 // Install does nothing
 func (d Dummy) Install() error { return nil }
